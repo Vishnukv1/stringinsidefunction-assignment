81d24772de5b8232ce119faf5fed5e7a0b0bae1c
Vishnukv1 <127046800+Vishnukv1@users.noreply.github.com>
2023-03-19 22:34:46 +0530

    Add files via upload

---
+++ stringinsidefunction.py
@@ -0,0 +1,49 @@
+a = input("Enter the string :", )
+def reverse(a):
+    reversed_a=a[::-1]
+    return reversed_a
+result = reverse(a)
+print("Reversed string is :",result )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
